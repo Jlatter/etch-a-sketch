3b3dd14188b88608616352b59852da1ea6fa7349
Jordan Latter <Jlatter1111@gmail.com>
2022-10-17 16:47:45 +0100

    Create container div, make fuction to make a grid of divs, create another function to add ids and eventlisteners to the child divs

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const container = document.getElementById("contain");
+
+function sketchsize(row, height){
+    container.style.setProperty('--grid-row', row);
+    container.style.setProperty('--grid-height', height);
+    for(let i =0; i<(row * height); i++){
+        let cell = document.createElement("div");
+        cell.innerText = (i+1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+sketchsize(16,16);
+var gridItems = document.getElementsByClassName("grid-item");
+for(let i=0;i<gridItems.length;i++){
+    gridItems[i].addEventListener("mouseenter" ,(event)=> cool());
+}
+function cool(){
+    console.log("correct");
+};
