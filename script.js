2412da86c739f0fa592182cac08b9ed7748a1968
Jordan Latter <Jlatter1111@gmail.com>
2022-10-29 00:02:04 +0100

    Rearrange the grid, buttons and title to be vertically aligned in the center add a sliding scale to determine the size of the grid, create an eraser button and a random colour button, add background colours to the body and contain classes

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 
 const container = document.getElementById("contain");
+var gridItems = document.getElementsByClassName("grid-item");
 
-function sketchsize(row, height){
-    container.style.setProperty('--grid-row', row);
-    container.style.setProperty('--grid-height', height);
-    for(let i =0; i<(row * height); i++){
-        let cell = document.createElement("div");
-        
-        container.appendChild(cell).className = "grid-item";
+var pix =  document.getElementById("myRange");
+var output = document.getElementById("demo");
+console.log(pix.value)
+output.innerHTML = pix.value;
+let gridSize = pix.value;
+pix.oninput = function() {
+output.innerHTML = this.value;
+clearGrid();
+sketchsize(parseInt(this.value),parseInt(this.value));
+};
+function clearGrid(){
+     while(gridItems.length > 0){
+        gridItems[0].parentNode.removeChild(gridItems[0]);
     };
 };
-sketchsize(16,16);
-var gridItems = document.getElementsByClassName("grid-item");
+//make grid
+function sketchsize(row, height){
+container.style.setProperty('--grid-row', row);
+container.style.setProperty('--grid-height', height);
+for(let i =0; i<(row * height); i++){
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+};
 for(let i=0;i<gridItems.length;i++){
     gridItems[i].setAttribute("id", "pixel"+i);
 }
 for(let i=0; i<gridItems.length; i++){
     document.getElementById("pixel" +i).addEventListener("mouseenter", event => cool(i));
-}
+};
+};
+
+sketchsize(16,16);
+
+
 function cool(e){
     console.log("correct" + (e+1));
-    document.getElementById("pixel" + e).className =" grid-item transformed";
+    document.getElementById("pixel" + e).className ="grid-item transformed";
     
 };
 const clear = document.getElementById("option");
@@ -31,3 +49,6 @@ function cleared(){
         document.getElementById("pixel"+i).classList.remove("transformed");
         
     }};
+function eraser(){
+    document.getElementById("eraser").className ="eraser active"
+}
